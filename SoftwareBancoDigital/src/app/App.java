bb1c8d02dc43b1b2d0c3aada84af85124470de01
Gustavo Morais <gustvmorais@gmail.com>
2023-04-26 22:13:49 -0300

    criando classe Emprestimo

--- SoftwareBancoDigital/src/app/App.java
+++ SoftwareBancoDigital/src/app/App.java
@@ -12,15 +12,20 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws ParseException {
-        Conta conta = new ContaCorrente(112,new Titular("gustavo","099811"),1000.0);
+        Titular cliente = new Titular("gustavo", "099811");
+        Conta conta = new ContaCorrente(112, cliente, 1000.0);
         conta.setSaldo(2500.00);
-        CartaoCredito itemComprado = new CartaoCredito(new Compras(new Date(),"mouse",100.00));
+        CartaoCredito itemComprado = new CartaoCredito(new Compras(new Date(), "mouse", 100.00));
         System.out.println(itemComprado.toString());
         System.out.println("");
 //        System.out.println(itemComprado.getSaldoDisponivel());
 //        System.out.println(itemComprado.getSaldoDisponivel());
-        Pagamento.realizarPagamentoFatura(400.00,conta,itemComprado);
-        System.out.print("Saldo restante: "+conta.getSaldo());
+        Pagamento.realizarPagamentoFatura(400.00, conta, itemComprado);
+        System.out.print("Saldo restante: " + conta.getSaldo());
+        Emprestimo emprestimo = new Emprestimo(1000.00, new Date(),12, cliente);
+        System.out.println("\n");
+        System.out.println(emprestimo.toString());
+
 
     }
 }
---
+++ SoftwareBancoDigital/src/module/entites/Emprestimo.java
@@ -0,0 +1,117 @@
+package module.entites;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Emprestimo {
+    private double valor;
+    private Date data;
+    private int prazo;
+    private double taxa;
+    private Titular cliente;
+    private String status;
+    private Integer quantidadeParcelas;
+    private Integer parcelaAtual =0;
+
+    public Emprestimo(double valor, Date data,int quantidadeParcelas, Titular cliente) {
+        this.valor = valor;
+        this.data = data;
+        this.prazo = prazo;
+        this.taxa = 0.06;
+        this.cliente = cliente;
+        this.status = "Em aberto";
+        this.quantidadeParcelas = quantidadeParcelas;
+    }
+
+    public double getValorEmprestimo() {
+        return valor;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public void setValorEmprestimo(double valor) {
+        this.valor = valor;
+    }
+
+    public double getTaxaJuros() {
+        return taxa;
+    }
+
+    public void setTaxaJuros(double taxa) {
+        this.taxa = taxa;
+    }
+
+    public double getValorJuros() {
+        return this.valor * this.taxa;
+    }
+
+    public Date getDataEmprestimo() {
+        return data;
+    }
+
+    public void setDataEmprestimo(Date data) {
+        this.data = data;
+    }
+
+    public double getValorTotal() {
+        return this.calcularValorParcelaParaPagar() * this.quantidadeParcelas;
+    }
+
+    public double getValorParcela() {
+        return calcularValorParcelaParaPagar();
+    }
+
+
+    public int getQuantidadeParcelas() {
+        return this.quantidadeParcelas;
+    }
+
+    public void setQuantidadeParcelas(int quantidade) {
+        this.prazo = quantidade;
+    }
+
+    public void pagarParcela(double valor) {
+
+        if (valor <= this.calcularValorParcelaParaPagar()) {
+            System.out.println("Saldo insuficiente!");
+        } else {
+            System.out.println("Parcela paga");
+            parcelaAtual++;
+            System.out.println(parcelaAtual);
+            if (parcelaAtual==this.quantidadeParcelas){
+                System.out.println("Todas as parcelas foram pagas");
+                this.parcelaAtual=0;
+            }
+
+        }
+    }
+
+    public boolean verificarEmprestimoPago() {
+        return status.equals("Pago");
+    }
+
+    private double calcularValorParcelaParaPagar() {
+        return (this.valor + (this.valor * taxa)) / 12;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("DADOS DO EMPRESTIMO: \n");
+        sb.append("Quantidade de parcelas: "+this.getQuantidadeParcelas()+"\n");
+        sb.append("Taxa de juros: "+this.getTaxaJuros()+"\n");
+        sb.append("Valor de cada parcela: "+String.format("R$ %.2f",this.getValorParcela())+"\n");
+        sb.append("Valor total das parcelas: R$ "+this.getValorTotal());
+
+        return sb.toString();
+    }
+
+
+    }
